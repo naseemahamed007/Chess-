14fa675d7e9f0ed314102f8bdbd7dd843d247dda
naseemahamed007 <naseemishere0@gmail.com>
2026-03-05 02:59:02 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,12 +1,38 @@
 tournaments = {
-    "open2026": {
-        "name": "Chennai Open Chess Championship",
+    "chennai_open_2026": {
+
+        "title": "Chennai Open Chess Tournament",
+        "organizer": "Nas Matrix Chess",
+
         "date": "28 March 2026",
         "mode": "Online",
         "platform": "Chess.com",
+        "format": "Rapid | Swiss System",
+        "time_control": "10 Minutes",
+
         "fee": "₹150",
-        "prize": "Winner & Runner-up Trophy",
-        "time": "10 Minutes Rapid",
-        "about": "Professional online tournament organized by Nas Matrix Chess."
+
+        "awards": [
+            "Medal for All",
+            "Certificate for All",
+            "Trophy for Winner & Runner-up"
+        ],
+
+        "about": """
+Professional online tournament promoting
+discipline, fair play, and excellence.
+""",
+
+        "team": {
+            "Director": "Naseem Ahamed",
+            "Organizer": "Omer Kose",
+            "Media": "Abuzer"
+        },
+
+        "contact": {
+            "phone": "7094602011",
+            "email": "naseemishere0@gmail.com",
+            "instagram": "@nasmatrixchess"
+        }
     }
 }
